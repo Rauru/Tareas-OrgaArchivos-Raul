be66476eaabcb1a28792802c672a050a14c124fe
Rauru <raul_lopez_tu@hotmail.com>
2020-08-18 20:16:38 -0600

    'commit'

---
+++ Ejercicio2/Ejer2_1/count_sort.c
@@ -0,0 +1,22 @@
+
+void countSort(int arr[], int n, int exp)
+{
+    int output[n]; // output array
+    int i, count[10] = { 0 };
+ 
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+ 
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+ 
+    // Build the output array
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+ 
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
